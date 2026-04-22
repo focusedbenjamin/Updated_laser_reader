a137b076ee79bd460b7f3ba23ba5f300b51c8b8a
Benjamin Mwangi <focusedbenjamin@gmail.com>
2026-04-22 00:48:20 +0000

    Ignore ROS build artifacts

--- build/exercises_unit_8/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/catkin_ws/src/exercises_unit_8
-# Build directory: /home/user/catkin_ws/build/exercises_unit_8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/laser_reader/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/catkin_ws/src/laser_reader
-# Build directory: /home/user/catkin_ws/build/laser_reader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
